2064489bfa7a7193c15b3961fff8a010c9a617b3
LimDBi <144079364+LimDBi@users.noreply.github.com>
2023-11-13 11:05:04 +0900

    Update constants.scala
    end.

--- FlappyBug/constants.scala
+++ FlappyBug/constants.scala
@@ -3,10 +3,13 @@ package o1.flappy
 // This file defines constants that are used in o1.flappy to define the rules of FlappyBug.
 
 val ObstacleSpeed = 10
-val FallingSpeed = 2
+val BugSpeed = 2
+val Gravity = 2
 val BugRadius = 15
-val FlapStrength = 15   //wingpower
+val FlapStrength = 15
 
 val ViewWidth = 1000
 val ViewHeight = 400
 val GroundDepth = 50
+val GroundY = ViewHeight - GroundDepth
+
